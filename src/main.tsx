b15dabb9937c9cbacd83ec984a160afbba10dd9a
zedonboy <zedonbiz@gmail.com>
2021-12-17 20:08:39 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import DW from "./pages/Dashboard"
@@ -10,7 +10,7 @@ import HomeDashboard from "./pages/dashboard/Home.dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/dashboard" element={<DW/>}>
@@ -19,7 +19,7 @@ ReactDOM.render(
       <Route path="/signin" element={<SignIn/>}/>
     </Routes>
       
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
